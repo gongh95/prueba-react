48eb9d89d92ff6f2c3a3f83f3e4a86b98ba46317
gongh95 <gonzalo.simon.1995@gmail.com>
2022-06-12 22:58:38 -0300

    creado componente contador

---
+++ prueba-react-22/src/Components/Contador.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import '../css/Contador.css'
+
+const Contador = () => {
+
+    const [contador, setContador] = useState(0);
+
+    const sumar = (prevState) => setContador(prevState => prevState +1);
+    const restar = (prevState) => {
+        if (contador === 0) {
+            return;
+        }
+        else {
+            setContador(prevState => prevState -1);
+        }
+    }
+
+    return (
+        <div className='contador'>
+            <h4 style={{ color: 'brown' }}>Componente contador</h4>
+            <button onClick={sumar}>+</button>
+            <p>{contador}</p>
+            <button onClick={restar}>-</button>
+        </div>
+    );
+}
+
+export default Contador;
--- prueba-react-22/src/Components/Main.js
+++ prueba-react-22/src/Components/Main.js
@@ -1,9 +1,12 @@
 import imagen from '../assets/img/react.svg'
 import '../css/Main.css'
+import Contador from './Contador';
 
 function Main() {
+
     return (
     <main className="main">
+          <Contador/>
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             Probando ReactJs :D
